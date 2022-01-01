1a42c5b9568294f1655f9331e7c3046ba4440d2b
Curtis Mattoon <cmattoon@cmattoon.com>
2022-01-01 18:01:50 +0000

    sig_handler

--- src/display.c
+++ src/display.c
@@ -5,15 +5,26 @@
  * - Bitcoind status
  * - LND/lncli status
  */
+#include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
 #include "ssd1306_i2c.h"
 #include "time.h"
 #include <unistd.h>
 
+// sig_handler clears the screen and exits
+void sig_handler(int sig) {
+  OLED_Clear();
+  signal(sig, SIG_DFL);
+  raise(sig);
+}
+
 int main(void) 
 {
   unsigned char symbol=0;
+
   ssd1306_begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS);
+
   if (i2cd<0)
     {
       printf("I2C device failed to open\r\n");
@@ -26,12 +37,15 @@ int main(void)
   //Get IP address
   FirstGetIpAddress();
 
+  signal(SIGINT, sig_handler);
+
   while(1)
     {
-      LCD_Display(symbol);
-      sleep(1);
-      sleep(1);
-      sleep(1);
+      if (LCD_Display(symbol)) {
+	sleep(1);
+	sleep(1);
+	sleep(1);
+      }
       symbol++;
 
       if(symbol==4)
